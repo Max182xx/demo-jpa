4097d6f72d7407d21e844f31374c02356ffe7b42
Max182xx <m.garciapro34@gmail.com>
2024-11-04 17:29:02 +0100

    TP01 demo JPA

--- src/main/java/fr/digi/m062024/App.java
+++ src/main/java/fr/digi/m062024/App.java
@@ -1,4 +1,15 @@
 package fr.digi.m062024;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class App {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("demo-jpa");
+        EntityManager em = emf.createEntityManager();
+        System.out.println(em);
+        em.close();
+        emf.close();
+    }
 }
